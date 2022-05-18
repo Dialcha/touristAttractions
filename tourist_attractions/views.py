8690f64f9d38049170b57b3366d244a0cee78a16
Diego <diego.chavarriap@gmail.com>
2022-05-18 00:43:06 -0500

    Add states and attractions with Forms

--- tourist_attractions/views.py
+++ tourist_attractions/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import StateCreateForm, AttractionCreateForm
+from django.views.generic.edit import CreateView
+from .models import State, Attraction
 
 # This is the dictionary for all the attractions
 attractions = [
@@ -19,3 +22,13 @@ def details(request, statename):
   # We replace the dash with a space for later ease of use. The dash is there because of the slugify filter.
   context = {"attractions" : attractions, "statename" : statename.replace("-", " ")}
   return render(request, 'tourist_attractions/details.html', context)
+
+class StateCreate(CreateView):
+  model = State
+  form_class = StateCreateForm
+  template_name = "tourist_attractions/state_create_form.html"
+
+class AttractionCreate(CreateView):
+  model = Attraction
+  form_class = AttractionCreateForm
+  template_name = "tourist_attractions/attraction_create_form.html"
